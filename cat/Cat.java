5eac747105db4e9c9f5b951ebfcf6f193533cb3e
Huy Tran <kingbazoka@gmail.com>
2024-01-02 00:45:17 -0800

    cat: step 2 - print from stdin

--- cat/Cat.java
+++ cat/Cat.java
@@ -1,8 +1,28 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 class Cat {
+  static void printFile(String fileName) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(fileName));
+    int c;
+    while ((c = reader.read()) != -1) {
+      System.out.print((char)c);
+    }
+    reader.close();
+  }
+
+  static void printStdin() throws IOException {
+    Scanner scanner = new Scanner(new InputStreamReader(System.in, "UTF-8"));
+    System.out.print(scanner.next());
+    while (scanner.hasNext()) {
+      System.out.print(" " + scanner.next());
+    }
+    scanner.close();
+  }
+
   public static void main(String[] args) {
     if (args.length <= 0) {
       System.out.println("Not enough arguments");
@@ -11,13 +31,14 @@ class Cat {
 
     String fileName = args[0];
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-      int c;
-      while ((c = reader.read()) != -1) {
-        System.out.print((char)c);
+    try {
+      if (fileName.equals("-")) {
+        printStdin();
+      } else {
+        printFile(fileName);
       }
-    } catch (IOException e) {
-      System.out.println("Cannot open file " + fileName);
+    } catch (Exception e) {
+      System.out.println("Cannot read input");
     }
   }
 }
--- cat/judge.sh
+++ cat/judge.sh
@@ -18,7 +18,7 @@ else
 fi
 
 echo "[#1b]\tShould handle non-existent file"
-expectOutput="Cannot open file foo.txt"
+expectOutput="Cannot read input"
 actual=$(./cat foo.txt)
 output=$(echo "$actual" | grep "$expectOutput")
 if [[ -z "$output" ]]; then
@@ -32,6 +32,18 @@ fi
 echo "[#2]\tShould print file content"
 expectOutput=$(cat test.txt)
 actual=$(./cat test.txt)
+output=$(echo "$actual" | grep "$expectOutput")
+if [[ -z "$output" ]]; then
+  echo "$FAILED"
+  echo "\tExpect: $expectOutput"
+  echo "\tActual: $actual"
+else
+  echo "$PASSED"
+fi
+
+echo "[#3]\tShould print from stdin"
+expectOutput=$(head -n1 test.txt | cat -)
+actual=$(head -n1 test.txt | ./cat -)
 output=$(echo "$actual" | grep "$expectOutput")
 if [[ -z "$output" ]]; then
   echo "$FAILED"
